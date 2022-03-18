f2c48b2ae2e3465d160c3730e5894da541cb89a6
Hadi F <62212402+hadi-f90@users.noreply.github.com>
2022-03-18 22:30:10 +0330

    Adding Product & Category models

--- starter/dog_shelters/models.py
+++ starter/dog_shelters/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.TextField()
+    price = models.DecimalField()
+    creation_date = models.DateField()
+
+
+class Category(models.Model):
+    name = models.TextField()
