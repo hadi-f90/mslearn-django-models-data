54d152f01719ca9e54611932efd4c179d2bcaccb
Hadi F <62212402+hadi-f90@users.noreply.github.com>
2022-03-19 18:18:57 +0330

    Adding some database tables, relationship, models, etc.

--- starter/dog_shelters/models.py
+++ starter/dog_shelters/models.py
@@ -1,11 +1,32 @@
+from unicodedata import category
 from django.db import models
 
 
+class Shelter(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Dog(models.Model):
+    shelter = models.ForeignKey(Shelter, on_delete=models.PROTECT)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    intake_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
-    name = models.TextField()
-    price = models.DecimalField()
-    creation_date = models.DateField()
+    name = models.TextField(max_length=50, min_length=3, unique=True)
+    price = models.DecimalField(min_value=0.99, max_value=1000)
+    creation_date = models.DateField(auto_now_add=True)
+    category = models.ForeignKey('Category',  # name the model
+                                 on_delete=models.PROTECT)
 
 
 class Category(models.Model):
-    name = models.TextField()
+    name = models.TextField(max_length=50, min_length=3, unique=True)
